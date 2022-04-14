229a70001ddfdaa771bf535fc01a7be4dbbeee29
mayurlalwani <lalwanimayur06@gmail.com>
2022-04-14 12:14:11 +0530

    add routes to app and wrap with state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import GithubState from "./context/github/githubState";
+import AlertState from "./context/alert/alertState";
+import Navbar from "./components/layouts/Navbar";
+import Alert from "./components/layouts/Alert";
+import HomePage from "./components/pages/HomePage";
+import User from "./components/users/User";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Github app</h1>
-    </div>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar title="Github" />
+            <div className="container">
+              <Alert />
+              <Routes>
+                <Route exact path="/" element={<HomePage />} />
+                <Route exact path="/user/:login" element={<User />} />
+              </Routes>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
 }
 
